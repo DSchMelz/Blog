f61ab3457d701d5f66987aff87b004f2bd535c04
DSchMelz <leshev.da@gmail.com>
2019-06-18 17:41:32 +0500

    Небольгие изменения кода ajax  и css

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -6,26 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
 		xhr.open("GET", "http://127.0.0.1:8000/ajax", true);//гет запрос по ссылке на страницу с текстом, запрос - ассинхронный(страница не будет останавливать весь код)
 
 		xhr.send();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState != 4) {//пока запрос не завершён(цифра 4 - завершён)
+		output.innerHTML = "Loading...";//пока загружается текст будет loading...
+		xhr.onreadystatechange = function() {//когда запрос завершился(хз чем отличается от события load(onload) и loadend(onloadend))
+			if (xhr.readyState != 4) {//если запрос не завершён(цифра 4 - завершён)
 				return
-			};
+			};//здесь оно ничего не делает, но может пригодиться
 			if (xhr.status != 200) {//200- все ок
 				// обработать ошибку
 				alert(xhr.status + ": " + xhr.statusText); // пример вывода: 404: Not Found
-			} 
+			}
 			else {
 				output.style.border = "1px solid #747474";
 				output.style.padding = "10%";
 				// вывести результат
 				output.innerHTML = xhr.responseText; // responseText -- текст ответа.
-			}
+				document.querySelector(".ajaxbtn").innerHTML = "Success!";
+			};
 		};
-		output.innerHTML = "Loading...";//пока загружается текст будет loading...
-		
-		xhr.onload = function() {//если запрос завершился успешно
-			document.querySelector(".ajaxbtn").innerHTML = "Success!";
-		}
 	
 		xhr.timeout = 30000; // 30 секунд (в миллисекундах)
 

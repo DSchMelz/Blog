f4817b18139660945e61d13b2150df276bc19a89
DSchMelz <leshev.da@gmail.com>
2019-09-17 14:01:30 +0300

    Удаление ненужных миксинов

--- blog/mixins.py
+++ blog/mixins.py
@@ -16,33 +16,3 @@ class ObjectDetailMixin:#испотуем миксины
         #     ['leshev_aa@mail.ru'],
         #     fail_silently=False,
         # )
-
-class ObjectUpdateMixin:
-    model = None
-    model_form = None
-    template = None
-    def get(self, request, slug):
-        obj = self.model.objects.get(slug__iexact=slug)# берём тэг по слагу из бд
-        bound_form = self.model_form(instance = obj)
-        return render(request, self.template, context={"form": bound_form, self.model.__name__.lower(): obj})
-    def post(self, request, slug):
-        obj = self.model.objects.get(slug__iexact=slug)# берём тэг по слагу из бд
-        bound_form = self.model_form(request.POST, instance = obj)
-
-        if bound_form.is_valid():
-            new_obj = bound_form.save()
-            return redirect(new_obj)
-        return render(request, self.template, context={"form": bound_form, self.model.__name__.lower(): obj})
-
-class ObjectDeleteMixin:
-    model = None
-    template = None
-    redirectUrl = None
-
-    def get(self, request, slug):
-        obj = self.model.objects.get(slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
-    def post(self, request, slug):
-        obj = self.model.objects.get(slug__iexact=slug)
-        obj.delete()
-        return redirect(reverse(self.redirectUrl))
